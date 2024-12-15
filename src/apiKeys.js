605455d8895c379d2cf0ee34c8ba18f6d9dff85c
jiya vyas <jiyavyas2811@gmail.com>
2024-12-15 15:40:44 +0530

    Intial Commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "9b4b5532451fbef8c4bf2c207fe8e59c",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
